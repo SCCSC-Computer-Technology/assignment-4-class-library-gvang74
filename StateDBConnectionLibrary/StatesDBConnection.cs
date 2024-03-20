3cb32209b87d289f51e48e39f9c0df10fee1c72a
gvang74 <156260586+gvang74@users.noreply.github.com>
2024-03-20 18:40:43 -0400

    Update Assignment 4
    Create a class library from your assignment two and upload it to GitHub. All you need is a library. No webform is needed here.  
     I Hope this is right.

---
+++ StateDBConnectionLibrary/StatesDBConnection.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using StateDBConnectionLibrary;
+using System.Data.SqlClient;
+
+namespace StateDBConnectionLibrary
+{
+    public static class StatesDBConnection
+    {
+        private static DataClasses1DataContext db = new DataClasses1DataContext();
+
+        //SqlConnection connect = new SqlConnection(@"Data Source =.\SQLEXPRESS; AttachDbFilename=C:\Users\gong_\Desktop\2024SP - CPT 206 - A80S - Adv Event-Driven Program\gvang_CPT_206_StateApp2\gvang_CPT_206_StateApp2\50statesDB.mdf;Integrated Security = True; Connect Timeout = 30");
+
+        //Disposes the database.
+        public static void DisposeDatabaseConnection() { db.Dispose(); }
+
+
+
+
+        //All State rows from the database</returns>
+        //public static StateTable[] GetAllStates() => db.StateTables.ToArray();
+
+
+
+        //All State rows from the database that match the parameters</returns>
+        public static StateTable[] StateName(string stateName)
+        {
+            var state = db.StateTables;
+            var result = stateName != "" ? state.Where((StateTable) => StateTable.StateName.Contains(stateName)) : state;
+            
+            return result.ToArray();
+        }
+        
+       
+    }
+}
+
+
